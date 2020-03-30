51d3c6359083af746dd2e4f246981817b6a6a127
jitender24 <jitenderpalsingh66@gmail.com>
2020-03-31 03:23:35 +0530

    created angular project & complete feature1.

---
+++ angular-bookstore/src/app/services/book.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Book } from '../common/book';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+private baseUrl = "http://localhost:8080/api/v1/books"
+  constructor(private htttpClient: HttpClient) { }
+
+  getBooks(): Observable<Book[]>{
+    return this.htttpClient.get<GetResponseBooks>(this.baseUrl).pipe(
+     map(response => response._embedded.books)
+    );
+    
+  }
+}
+
+interface GetResponseBooks{
+  _embedded: {
+    books: Book[];
+  }
+}
--- onlinebookStore/onlinebookStore/src/main/java/com/bookStore/onlinebookStore/repository/BookRepository.java
+++ onlinebookStore/onlinebookStore/src/main/java/com/bookStore/onlinebookStore/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package com.bookStore.onlinebookStore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.bookStore.onlinebookStore.entity.Book;
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
 }
